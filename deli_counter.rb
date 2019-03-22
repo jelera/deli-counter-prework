709d1bc83770b169d139fd6ab2ceebb6ef61fdbc
Jose Elera <jelera@gmail.com>
2019-03-22 19:57:27 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,11 +11,11 @@ def line(deli_line)
     deli_line.each_with_index do |person, i|
       line_queue << " #{i + 1}. #{person}"
     end
-    
+
     puts "#{line_msg}:#{line_queue}"
-    
+
   end
-  
+
 end
 
 def take_a_number(deli_line, person)
