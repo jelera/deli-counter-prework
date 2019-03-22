c3ba3c81b21e0f5f199156fe4cd5398043c5e3c3
Jose Elera <jelera@gmail.com>
2019-03-22 19:29:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,18 +2,18 @@
 def line(deli_line)
 
   line_msg = "The line is currently"
-  
+
   if deli_line.length == 0
     "#{line_msg} empty"
   else
     line_queue = ""
-    
+
     deli_line.each_with_index do |person, i|
       line_queue << "#{i + 1}. #{person}"
     end
     "#{line_msg}:"
   end
-  
+
 end
 
 def take_a_number
